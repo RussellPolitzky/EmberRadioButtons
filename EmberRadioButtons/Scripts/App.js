2cee96b5e919a264e853cef13b04f6191e5295d1
russell <russell.politzky@dariel.co.za>
2012-12-15 17:30:17 +0200

    About to add more buttons.

--- EmberRadioButtons/Scripts/App.js
+++ EmberRadioButtons/Scripts/App.js
@@ -1,5 +1,57 @@
-﻿/// <reference path="../Libs/JQuery/jquery.js" />
-/// <reference path="../Libs/Handlebars/handlebars.js" />
-/// <reference path="../Libs/Ember/ember.js" />
+var App = Ember.Application.create();
 
-var App = Ember.Application.create(); 
+
+App.Model = Ember.Object.extend({
+    name: 'John',
+    state: 'dn'
+});
+
+App.ApplicationController = Ember.Controller.extend({
+    content: App.Model.create(),
+    changeName: function () {
+        var name = this.get('content.name');
+        this.set('content.name', name + '1');
+    }
+});
+
+
+App.ToggleButton = Ember.View.extend({
+    pressed: false,
+    tagName: 'button',               // This view is a button.
+    classNames: ['up'],              // Default class for the button.
+    classNameBindings: ['_pressed'], // Bind the class name to 'pressed'
+    
+    // Allow these attributes to be bound in the view.
+    attributeBindings: ['href', 'target', 'rel', 'tabindex', 'action', 'pressed'],
+
+    _pressed: function () {
+        var pressed = this.get('pressed');
+        if (pressed) { return 'dn'; } else { return 'up'; }
+    } .property('pressed'),
+
+    click: function (event) {
+        var pressed = this.get('pressed');
+        this.set('pressed', !pressed);
+    }
+});
+
+
+App.ApplicationView = Ember.View.extend({
+  templateName: 'application'
+});
+
+
+App.Router = Ember.Router.extend({
+    root: Ember.Route.extend({
+        index: Ember.Route.extend({
+            route: '/'
+        }),
+
+        // Event handlers.
+        changeName: function (router) {
+            router.get('applicationController').changeName(); // Router delegates to the controller.
+        }
+    })
+});
+
+App.initialize();
--- EmberRadioButtons/Scripts/PeopleController.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="../Libs/Ember/ember.js" />
-/// <reference path="Person.js" />
-
-App.peopleController = Em.ArrayController.create({
-    content: App.Person.creat()
-});
