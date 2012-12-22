442a04347b773f51ec084ab53e868bc440e4f48c
russell <russell.politzky@dariel.co.za>
2012-12-22 13:49:51 +0200

    Got the ember sample working.

--- EmberRadioButtons/Scripts/NewButtonModel.js
+++ EmberRadioButtons/Scripts/NewButtonModel.js
@@ -5,13 +5,47 @@
 // set of radio buttons.
 //------------------------------------------------
 App.NewButtonModel = Ember.Object.extend({
-    
-    buttons: undefined,
+
+    _previousButton: undefined,
+
+    buttons: [
+        Ember.Object.create({ pressed: true }),
+        Ember.Object.create({ pressed: false }),
+        Ember.Object.create({ pressed: false })
+    ],
 
     init: function() {
-        var buttonArrayProxy = Ember.ArrayProxy.create({
-            content: Ember.A([ { pressed: true }, { pressed: false }, { pressed: false } ])
-        });
-        this.set('buttons', buttonArrayProxy);
-    }
-});
+        /// <summary>
+        /// 
+        /// </summary>
+
+
+        //var buttonArrayProxy = Ember.ArrayProxy.create({
+        //    content: Ember.A([{ pressed: true }, { pressed: false }, { pressed: false }])
+        //});
+
+        this.set('_previousButton', 0);
+      
+        //this.set('buttons', buttonArrayProxy);
+    },    
+
+    setState: function(arg) {
+        /// <summary>
+        /// 
+        /// </summary>
+        if (this.get('buttons')) {
+            var previousButtonNumber = this.get('_previousButton');
+            var buttons = this.get('buttons');
+            buttons.objectAt(previousButtonNumber).set('pressed', false);
+            var index = 0;
+            var found = false;
+            
+            while (!found && index < buttons.length) {
+                found = buttons.objectAt(index).get('pressed');
+                index = index + 1;
+            }
+            var foundIndex = index - 1;
+            this.set('_previousButton', foundIndex);
+        }
+    }.propertyobserves('buttons.@each.pressed')
+})
