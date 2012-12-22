a99749bfb2d35fdc6bae1df17223c27605b4364f
russell <russell.politzky@dariel.co.za>
2012-12-22 18:56:27 +0200

    logic is all correct now.

--- EmberRadioButtons/Scripts/NewButtonModel.js
+++ EmberRadioButtons/Scripts/NewButtonModel.js
@@ -7,6 +7,8 @@
 App.NewButtonModel = Ember.Object.extend({
    
     _previousButton: undefined,
+    
+    numberOfButtons: undefined,
 
     buttons: undefined,
 
@@ -16,7 +18,13 @@ App.NewButtonModel = Ember.Object.extend({
                 Ember.Object.create({ pressed: false }),
                 Ember.Object.create({ pressed: false })
         ]);
+        this._setNumberOfButtons();
     },    
+
+    _setNumberOfButtons: function() {
+        this.set('numberOfButtons', this.get('buttons').length);
+    },
+
 
     addButton: function() {
         this.get('buttons').pushObject(Ember.Object.create({ pressed: false }));
@@ -33,9 +41,19 @@ App.NewButtonModel = Ember.Object.extend({
 
         var previousButtonNumber,
             buttons,
-            index,
-            found,
-            foundIndex;
+            index = 0,
+            found = false,
+            foundIndex,
+            buttonsDefined = function() {
+                return this.get('buttons');
+            };
+
+        // if buttons have been added, then don't do any of this.
+        if (this.get('buttons').length != this.get('numberOfButtons')) {
+            this._setNumberOfButtons();
+            return;
+        }
+
 
         if (this.get('buttons')) {
             buttons = this.get('buttons');
@@ -45,13 +63,15 @@ App.NewButtonModel = Ember.Object.extend({
                 buttons.objectAt(previousButtonNumber).set('pressed', false);
             }
             
-            index = 0;
-            found = false;
+            //index = 0;
+            //found = false;
             while (!found && index < buttons.length) {
                 found = buttons.objectAt(index).get('pressed');
                 index = index + 1;
             }
             foundIndex = index - 1;
+            
+
             if (found) {
                 this.set('_previousButton', foundIndex);
             } else {
--- EmberRadioButtons/Scripts/Router.js
+++ EmberRadioButtons/Scripts/Router.js
@@ -2,7 +2,13 @@
 
 //------------------------------------------------
 // Router with a single route.
-// (this is a single page application)
+// This is a single page application and in this
+// case there is only one state,  but this is the 
+// general form of an Ember application.  From 
+// a patterns point of view, the router is a 
+// hierarchical state machine and isn't quite a 
+// full state-chart in that it lacks support
+// for orthogonal states.
 //------------------------------------------------
 App.Router = Ember.Router.extend({
     root: Ember.Route.extend({
@@ -12,20 +18,25 @@ App.Router = Ember.Router.extend({
 
         // Event handlers.
         changeName: function (router) {
+            // Delegates gto the controller.
             router.get('applicationController').changeName(); // Router delegates to the controller.
         },
 
         addButton: function (router) {
+        	/// <summary>
+        	/// Delegates to controller
+        	/// </summary>
+        	/// <param name="router"></param>
             router.get('applicationController').addButton();
         },
-        
-
 
         removeButton: function (router) {
+        	/// <summary>
+        	/// Delegates to controller/
+        	/// </summary>
+        	/// <param name="router"></param>
             router.get('applicationController').removeButton();
         }
-        
-
     })
 });
 
